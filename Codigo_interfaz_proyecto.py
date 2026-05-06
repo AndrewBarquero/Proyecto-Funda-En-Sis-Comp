f9b283d621666d6a563eb02cbfed4e250256a011
AndrewBarquero <a.barquero.6@estudiantec.cr>
2026-05-06 15:31:34 -0600

    El codigo con el modo de transmision casi al 100

--- Codigo_interfaz_proyecto.py
+++ Codigo_interfaz_proyecto.py
@@ -1,170 +1,256 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-import time
-import serial
 import random
+import serial
+import time
 
-ser = serial.Serial('COM3', 115200) #Puerto
+# ===================== SERIAL =====================
+ser = serial.Serial('COM3', 115200, timeout=1)
+time.sleep(2)
 
-
-
-
-#---------------------------Ventana principal----------------------------------------------
+# ===================== VENTANA PRINCIPAL =====================
 ventana_principal = tk.Tk()
-ventana_principal.title("Pantalla Principal")
+ventana_principal.title("Pantalla principal")
 ventana_principal.geometry("575x425+650+200")
 ventana_principal.resizable(False, False)
 
-imagen1 = Image.open("Pantalla principal.PNG")  
+imagen1 = Image.open("Pantalla principal.PNG")
 imagen_tk = ImageTk.PhotoImage(imagen1)
 imagen_inicio = tk.Label(ventana_principal, image=imagen_tk)
 imagen_inicio.pack()
-#------------------------------------------------------------------------------------
 
+# ===================== CIERRE SEGURO =====================
+def cerrar_todo():
+    try:
+        ser.write(b"OFF\n")
+        ser.flush()
+        time.sleep(0.2)
+        ser.close()
+    except:
+        pass
+    ventana_principal.destroy()
 
+ventana_principal.protocol("WM_DELETE_WINDOW", cerrar_todo)
 
-
-
-
-#------------------------Ventana donde se seleciona el modo de juego ----------------------
+# ===================== MODOS DE JUEGO =====================
 def modo_de_juego():
     ventana_principal.withdraw()
 
     ventana_juego1 = tk.Toplevel(ventana_principal)
     ventana_juego1.title("Modos de juego")
     ventana_juego1.geometry("515x375+675+250")
-    ventana_juego1.resizable(False, True)
+    ventana_juego1.resizable(False, False)
 
-        #----------------------------Ventana del juego de transmision-----------------------
+
+
+
+    #-------------------------------------------------------Juego de transmision---------------------------------------------------
     def transmision():
+
         ventana_juego1.withdraw()
 
-        ventana_transmision = tk.Tk()
-        ventana_transmision.title("Modo transmision")
+        ventana_transmision = tk.Toplevel(ventana_principal)
+        ventana_transmision.title("Modo transmisión")
         ventana_transmision.geometry("500x400+650+200")
         ventana_transmision.resizable(False, False)
 
-        
+        #Texto del juego para dar instrucciones y mostrar puntaje 
+        texto_juego = tk.Label(
+            ventana_transmision,
+            text="Presiona Start para comenzar",
+            bg="RoyalBlue4",
+            fg="White",
+            font=('Arial', 10, 'bold')
+        )
+        texto_juego.pack(pady= 150)
 
-        texto_juego_transmision = tk.Label(ventana_transmision, text="Cuando presione el boton empezara el juego")
-        texto_juego_transmision.config(bg="RoyalBlue4", fg="SlateGray3", font=('Arial', 10, 'bold'))
-        texto_juego_transmision.place(relx=0.15, rely=0.4)
+        #Posibles palabras que se escucharan en morse
+        palabras = [
+            "SOS","NO","SI","CAJA","GATO","SOL","RATON","LAPIZ","ZAPATO"
+            "RELOJ","REGLA","HORROR","BOMBA","TRAMPA","PANTALLA","CELULAR",
+            "CELULA","SIETE","GRANDE","CABALLO","SAL","MANGO", "FOTO"
+            "AZUL","ARBOL","INGENIERIA","VOLTAJE","CORRIENTE","INVENCIBLE"
+            ]
 
-        palabras_juego_transmision = ["SOS","NO","SI","CAJA", "GATO", "SOL", "RATON", "LAPIZ", "RELOJ", "REGLA", "BOMBA", "PANTALLA", "CELULAR", 
-                                      "SIETE", "GRANDE", "CABALLO", "SAL", "MANGO", "AZUL", "ARBOL", "INGENIERIA","VOLTAJE", "CORRIENTE"]
         
+        fases = [
+            {"fase": 1, "msg": "FASE 1 - Velocidad normal"},
+            {"fase": 2, "msg": "FASE 2 - Más rápido"},
+            {"fase": 3, "msg": "FASE 3 - Máxima velocidad"}
+        ]
+
+        fase_actual = 0
+        palabra_actual = ""
+        puntaje_total = 0
+
+        entrada = tk.Entry(ventana_transmision, width=20)
+        entrada.place(relx=0.37, rely=0.5)
+
+        # ---------- LÓGICA DEL JUEGO ----------
         def juego_transmision():
-            texto_juego_transmision.config(text="¿listo?")
-            time.sleep(2)
-            texto_juego_transmision.config(text="3")
-            time.sleep(2)
-            texto_juego_transmision.config(text="2")
-            time.sleep(2)
-            texto_juego_transmision.config(text="1")
+            nonlocal fase_actual, palabra_actual
 
-   
+            #Pregunta si ya termino las 3 fases del juego, si terminaron las 3 fases, lo lleva a la ventana del puntaje
+            if fase_actual >= len(fases):
+                pedir_nombre()
+                return
 
-            
-        boton_start = tk.Button(ventana_transmision, width=5 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White" , text = "Start", command=juego_transmision).place(relx=0.42, rely=0.2)
+            fase = fases[fase_actual]
+            texto_juego.config(text=fase["msg"])
+            ventana_transmision.update()
+
+            #Envia la fase a la rasberry para la velocidad del juego
+            ser.write(f"FASE{fase['fase']}\n".encode())
+            time.sleep(1)
+
+            #Se escoje una palabra random del repertorio de posibles palabras
+            palabra_actual = random.choice(palabras)
+            entrada.delete(0, tk.END) #Limpia la entrada para escribir la siguiente palabra
+
+            ser.write(b"OFF\n")
+            time.sleep(0.1)
+            ser.write((palabra_actual + "\n").encode())
+
+            texto_juego.config(text="Escucha el Morse...")
+            fase_actual += 1 #Pasa a la siguiente fase
         
-        texto_juego_transmision
-        texto_juego_transmision
 
+        def analisis_puntaje(): #Funcion para valorar que tan parecida es la palabra que se dijo con la que el usuario cree que es
+            nonlocal puntaje_total
+            respuesta = entrada.get().upper() #Toma la palabra que se escribe en la entrada (la pasa a mayusculas)
+            puntaje_fase = 0
 
+            for i in range(min(len(palabra_actual), len(respuesta))): #Toma el rango de la palabra mas corta para que no se tomen espacios vacios
+                if palabra_actual[i] == respuesta[i]:
+                    if fase_actual == 1:
+                        puntaje_fase += 10
+                    elif fase_actual == 2:
+                        puntaje_fase += 15
+                    elif fase_actual == 3:
+                        puntaje_fase += 20
 
+            puntaje_total += puntaje_fase
+            texto_juego.config(
+                text=f"Puntaje fase: {puntaje_fase}\nPuntaje total: {puntaje_total}"
+            )
 
+        # ---------- GUARDAR PUNTAJE ----------
+        def pedir_nombre(): #Funcion que abre una ventana cuando acaba el juego y le pide el nombre al usuario para guardar su puntaje
+            ventana_nombre = tk.Toplevel(ventana_transmision)
+            ventana_nombre.title("Guardar puntaje")
+            ventana_nombre.geometry("300x150+500+400")
+            ventana_nombre.resizable(False, False)
 
+            tk.Label(
+                ventana_nombre,
+                text=f"Puntaje final: {puntaje_total}",
+                font=("Arial", 12, "bold")
+            ).pack(pady=10)
 
-        def volver_modo():
+            tk.Label(ventana_nombre, text="Nombre del jugador:").pack()
+            entrada_nombre = tk.Entry(ventana_nombre)
+            entrada_nombre.pack(pady=5)
+
+            def guardar(): #Funcion que toma el nombre del usuario y su puntaje y los agrega a la .txt  con las lista de usuarios
+                nombre = entrada_nombre.get().strip()
+                if nombre:
+                    with open("puntajes.txt", "a", encoding="utf-8") as f:
+                        f.write(f"{nombre},{puntaje_total}\n")
+                    ventana_nombre.destroy()
+
+            tk.Button(ventana_nombre, text="Guardar", command=guardar).pack(pady=10)
+
+        
+        def volver():
+            ser.write(b"OFF\n")
+            ser.flush()
             ventana_transmision.destroy()
             ventana_juego1.deiconify()
-        ventana_transmision.protocol("WM_DELETE_WINDOW", volver_modo)
 
-        boton_cierre_transimision = tk.Button(ventana_transmision, width=5 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White" , text = "Volver", command=volver_modo).place(relx=0.42, rely=0.8)
+        ventana_transmision.protocol("WM_DELETE_WINDOW", volver)
+        
+        
+        #---------Botones del interfaz------------
+        tk.Button(
+            ventana_transmision,
+            text="Start",
+            bg="RoyalBlue4",
+            fg="White",
+            width=8,
+            command=juego_transmision
+        ).place(relx=0.43, rely=0.2)
 
+        tk.Button(
+            ventana_transmision,
+            text="Calcular puntaje",
+            bg="RoyalBlue4",
+            fg="White",
+            width=15,
+            command=analisis_puntaje
+        ).place(relx=0.39, rely=0.65)
 
-    #-----------------------------------------------------------------------------------
-
-
-    imagen2 = Image.open("Fondo.PNG")  
-    tk_fondo = ImageTk.PhotoImage(imagen2)
-    imagen_fondo = tk.Label(ventana_juego1, image=tk_fondo)
-    imagen_fondo.image = tk_fondo
-    imagen_fondo.pack()
-
-    titulo_modo_de_juego = tk.Label(imagen_fondo, width=5 , text="SELECCIONE EL MODO DE JUEGO")
-    titulo_modo_de_juego.config(height=2, width=28, bg="RoyalBlue4", fg="SlateGray3", font=('Arial', 15, 'bold'))
-    titulo_modo_de_juego.place(relx=0.15, rely=0.1)
-
-    boton_modo_1 = tk.Button(imagen_fondo, width=20 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White" , text = "Modo Transmision Simple", command=transmision ).place(relx=0.31, rely=0.3)
-    boton_modo_2 = tk.Button(imagen_fondo, width=22 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White" , text = "Modo Escucha y Transmision", ).place(relx=0.30, rely=0.5)
-
-
-    def volver1():
-        ventana_juego1.destroy()
-        ventana_principal.deiconify()
-    ventana_juego1.protocol("WM_DELETE_WINDOW", volver1)
-
-    boton_cierre2 = tk.Button(imagen_fondo, width=5 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White" , text = "Volver", command=volver1).place(relx=0.42, rely=0.8)
-#--------------------------------------------------------------------------------------
-
-
-
-
-
-""" -------------------------------------------------Codigos utiles ----------------
-    def enviar_texto():
-        texto = entrada.get()
-        texto = texto.upper()
-        ser.write((texto + "\n").encode())
-
-    entrada = tk.Entry(imagen_fondo,width=30)
-    entrada.place(relx=0.42, rely=0.5)
-
-    boton_buzz = tk.Button(imagen_fondo,text="Enviar a Raspberry",command=enviar_texto)
-    boton_buzz.place(relx=0.42, rely=0.6)
-
-"""    
-"""
-    #Las siguientes funciones y botones hechos por Chatcito:
-    def encender():
-        ser.write(b'ON\n')
-
-    def apagar():
-        ser.write(b'OFF\n')
-
-    btn1 = tk.Button(imagen_fondo, text="Encender", command=encender)
-    btn1.place(relx=0.42, rely=0.3)
-
-    btn2 = tk.Button(imagen_fondo, text="Apagar", command=apagar)
-    btn2.place(relx=0.42, rely=0.5)
-"""#---------------------------------------------------------------------------------------
-
-
-#------------------------Ventana acerca de nosotros---------------------------------------
-
-def info():
-    ventana_principal.withdraw()
-
-    ventana_informacion = tk.Toplevel(ventana_principal)
-    ventana_informacion.title("Seccion de Analisis")
-    ventana_informacion.geometry("400x550+725+300")
-    ventana_informacion.resizable(False, True)
-
-#-------------------------------------------------------------------------------------------
+        tk.Button(
+            ventana_transmision,
+            text="Volver",
+            bg="RoyalBlue4",
+            fg="White",
+            width=8,
+            command=volver
+        ).place(relx=0.43, rely=0.8)
+    #------------------------------------------------------Fin juego de transmision----------------------------------------------------
 
 
 
 
+    imagen2 = Image.open("Fondo.PNG")
+    fondo_tk = ImageTk.PhotoImage(imagen2)
+    fondo = tk.Label(ventana_juego1, image=fondo_tk)
+    fondo.image = fondo_tk
+    fondo.pack()
 
-#------------------Botones pantalla principal------------------------------------------------
-boton_play = tk.Button(imagen_inicio,width=6 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White", text="Jugar", command=modo_de_juego).place(relx= 0.41, rely=0.45)
-boton_info = tk.Button(imagen_inicio,width=15 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White", text="Acerca de nosotros", command=info).place(relx= 0.36, rely=0.6)
-boton_cierre1 = tk.Button(imagen_inicio, width=5 ,relief="groove", bd=10,bg="RoyalBlue4",fg="White" , text = "Salir", command=ventana_principal.destroy).place(relx=0.42, rely=0.8)
-#------------------------------------------------------------------------------------------
+    tk.Button(
+        fondo,
+        text="Modo Transmisión Simple",
+        bg="RoyalBlue4",
+        fg="White",
+        width=22,
+        command=transmision
+    ).place(relx=0.3, rely=0.35)
 
+    tk.Button(
+        fondo,
+        text="Volver",
+        bg="RoyalBlue4",
+        fg="White",
+        width=8,
+        command=lambda: (ventana_juego1.destroy(), ventana_principal.deiconify())
+    ).place(relx=0.42, rely=0.8)
 
+# ===================== BOTONES PRINCIPALES =====================
+tk.Button(
+    imagen_inicio,
+    text="Jugar",
+    bg="RoyalBlue4",
+    fg="White",
+    width=6,
+    command=modo_de_juego
+).place(relx=0.41, rely=0.45)
+
+tk.Button(
+    imagen_inicio,
+    text="Salir",
+    bg="RoyalBlue4",
+    fg="White",
+    width=6,
+    command=cerrar_todo
+).place(relx=0.42, rely=0.8)
 
 ventana_principal.mainloop()
 
-
+"""
+Comandos para guardar a gitgub:
+1- git status
+2- git add .
+3- git commit -m "Mensaje"
+4- git push -n origin main
+"""
